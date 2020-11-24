3b802a82a9b278f5c41323a2c8c9b9e94492f9b7
MikeYef96 <mike.yef@gmail.com>
2020-11-24 21:49:42 +0300

    HW6 2v

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,19 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// const { height: pageHeaderHeight } = document
+//             .querySelector("page-header").getBoundingClientRect();
+//         document.body.style.paddingTop = '${ pageHeaderHeight }px';
+
+//FIXED HEADER^^^///////////////////////////////////////////////////////////
+
+// (() => {
+//   document.querySelector("js-speaker-form").addEventListener("submit", e => {
+//     e.preventDefault();
+
+//     new FormData(e.currentTarget).forEach((value, name) => 
+//       console.log("${name}: {value}"),
+//     );
+//   });
+// })();
